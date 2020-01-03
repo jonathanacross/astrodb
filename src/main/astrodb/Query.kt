521fedb3a474fc9d675a5aaf515974fed8a4e52f
Jonathan Cross <jonathan.a.cross@gmail.com>
2020-01-02 22:11:32 -0800

    adding other filters

--- src/main/astrodb/Query.kt
+++ src/main/astrodb/Query.kt
@@ -30,7 +30,7 @@ fun tokenize(query: String): List<String> {
 }
 
 fun parseCons(cons: String): List<Constellation> {
-    return cons.split(",").map{ c -> Constellation.parse(c)}
+    return cons.split(",").map { c -> Constellation.parse(c) }
 }
 
 fun parseQuery(query: String): ObjectFilter {
@@ -39,15 +39,21 @@ fun parseQuery(query: String): ObjectFilter {
     var nameIs: String? = null
     var nameLike: String? = null
     var conIn: List<Constellation> = emptyList()
+    var conNotIn: List<Constellation> = emptyList()
     var objectTypesIn: List<ObjectType> = emptyList()
+    var objectTypesNotIn: List<ObjectType> = emptyList()
     var raInRange: RaRange? = null
     var decGreaterThan: Double? = null
     var decLessThan: Double? = null
     var brighterThanMagnitude: Double? = null
+    var sbBrighterThan: Double? = null
     var sizeGreaterThan: Double? = null
     var sizeLessThan: Double? = null
-    var inProgram: String? = null
+    var programIs: String? = null
+    var programIn: List<String> = emptyList()
+    var programLike: String? = null
     var seen: Boolean? = null
+    var notSeenSince: String? = null
 
     var idx = 0
     while (idx < tokens.size) {
@@ -55,16 +61,21 @@ fun parseQuery(query: String): ObjectFilter {
             Keyword.NAME_IS -> { nameIs = tokens[idx + 2]; idx += 3 }
             Keyword.NAME_LIKE -> { nameLike = tokens[idx + 2]; idx += 3 }
             Keyword.CON_IN -> { conIn = parseCons(tokens[idx + 2]); idx += 3}
+            Keyword.CON_NOTIN -> { conNotIn = parseCons(tokens[idx + 2]); idx += 3}
             Keyword.TYPE_IN -> { objectTypesIn = ObjectType.parse(tokens[idx + 2]); idx += 3}
+            Keyword.TYPE_NOTIN -> { objectTypesNotIn = ObjectType.parse(tokens[idx + 2]); idx += 3}
             Keyword.RA_RANGE -> { raInRange = RaRange(parseBase60(tokens[idx + 2]), parseBase60(tokens[idx + 4])); idx += 5 }
             Keyword.DEC_GREATER -> { decGreaterThan = parseBase60(tokens[idx + 2]); idx += 3 }
             Keyword.DEC_LESS -> { decLessThan = parseBase60(tokens[idx + 2]); idx += 3 }
             Keyword.MAGNITUDE_LESS -> { brighterThanMagnitude = tokens[idx + 2].toDouble(); idx += 3 }
+            Keyword.SB_LESS -> { sbBrighterThan = tokens[idx + 2].toDouble(); idx += 3 }
             Keyword.SIZE_GREATER -> { sizeGreaterThan = Size.parse(tokens[idx + 2]).asNumber(); idx += 3 }
             Keyword.SIZE_LESS -> { sizeLessThan = Size.parse(tokens[idx + 2]).asNumber(); idx += 3 }
-            Keyword.PROGRAM_IS -> { inProgram = tokens[idx + 2]; idx += 3 }
-            Keyword.SEEN_IS -> { seen = tokens[idx + 2].toBoolean(); idx += 3}
-            else -> throw ParseException("not implemented")
+            Keyword.PROGRAM_IS -> { programIs = tokens[idx + 2]; idx += 3 }
+            Keyword.PROGRAM_IN -> { programIn = tokens[idx + 2].split(","); idx += 3 }
+            Keyword.PROGRAM_LIKE -> { programLike = tokens[idx + 2]; idx += 3 }
+            Keyword.SEEN_IS -> { seen = tokens[idx + 2].toBoolean(); idx += 3 }
+            Keyword.NOTSEEN_SINCE -> { notSeenSince = tokens[idx + 2]; idx += 3}
         }
     }
 
@@ -72,15 +83,21 @@ fun parseQuery(query: String): ObjectFilter {
         nameIs = nameIs,
         nameLike = nameLike,
         conIn = conIn,
+        conNotIn = conNotIn,
         objectTypesIn = objectTypesIn,
+        objectTypesNotIn = objectTypesNotIn,
         raInRange = raInRange,
         decGreaterThan = decGreaterThan,
         decLessThan = decLessThan,
         brighterThanMagnitude = brighterThanMagnitude,
+        sbBrighterThan = sbBrighterThan,
         sizeGreaterThan = sizeGreaterThan,
         sizeLessThan = sizeLessThan,
-        inProgram = inProgram,
-        seen = seen
+        programIs = programIs,
+        programIn = programIn,
+        programLike = programLike,
+        seen = seen,
+        notSeenSince = notSeenSince
     )
 }
 
@@ -88,14 +105,19 @@ enum class Keyword(val token1: String, val token2: String) {
     NAME_IS("name", "="),  // name like x, name = x
     NAME_LIKE("name", "like"),  // name like x, name = x
     CON_IN("con", "in"),   // con in and,per
+    CON_NOTIN("con", "notin"),   // con notin and,per
     TYPE_IN("type", "in"),  // type in ast,double
+    TYPE_NOTIN("type", "notin"),  // type notin ast,double
     RA_RANGE("ra", "range"), // ra range 23:00 to 2:00
     DEC_LESS("dec", "<="),  // dec >= 30, dec <= 20
     DEC_GREATER("dec", ">="),  // dec >= 30, dec <= 20
     MAGNITUDE_LESS("mag", "<="),  // magnitude <= 12
+    SB_LESS("sb", "<="),  // sb <= 12
     SIZE_LESS("size", "<="),  // size <= 1deg
     SIZE_GREATER("size", ">="),  // size >= 1deg
     PROGRAM_IS("program", "="), // program = "RASC.."
+    PROGRAM_IN("program", "in"), // program in "RASC..,500 Best.."
+    PROGRAM_LIKE("program", "like"), // program like "RASC"
     SEEN_IS("seen", "="),  // seen = true, seen = false
     NOTSEEN_SINCE("notseen", "since");  // notseen since 2019-01-01
 
