1942d2a78665c5079e8b3086916a7e6e20aa0912
Jonathan Cross <jonathan.a.cross@gmail.com>
2021-05-21 12:58:36 -0700

    updating tools to work with new file formats
    fixing some bad dates in programs

--- tools/progress.py
+++ tools/progress.py
@@ -2,8 +2,7 @@
 Generates a graph of number of objects observed in a program over time.
 Example:
 python3 progress.py \
-        -p data/programs.tsv \
-        -b data/observations.tsv \
+        -p ../data/programs.tsv \
         -n "Messier OP" \
         -o messier.pdf
 """
@@ -16,37 +15,29 @@ import matplotlib.pyplot as plt
 from optparse import OptionParser
 
 
-def get_item_names(program_file, program_name):
+def parse_date(observation_id):
+    'Parses an id like 20190129-02-m81m82 to a date, e.g., 2019-01-29.'
+    return observation_id[0:4] + "-" + observation_id[4:6] + "-" + observation_id[6:8]
+
+
+def get_dates_seen(program_file, program_name):
+    'Creates a map of date -> num observations for the given program.'
     pfile = open(program_file, 'r')
     lines = pfile.readlines()
-    names = set({})
+    seen_set = set({})
+    date_to_count = {}
     for line in lines:
         fields = line.strip().split('\t')
         program = fields[0]
         object_name = fields[2]
-        if program == program_name:
-            names.add(object_name)
-    return names
-
-
-def get_dates_seen(obs_file, object_names):
-    ofile = open(obs_file, 'r')
-    lines = ofile.readlines()
-    seen_set = set({})
-    date_to_count = {}
-    for line in lines:
-        if line.startswith("#"):
-            continue
-        fields = line.strip().split('\t')
-        date = fields[0]
-        object_name = fields[1]
-        seen_set.add(object_name)
-        num_seen_in_program = len(object_names.intersection(seen_set))
-        # there may be multiple entries per day, but since the count
-        # is strictly increasing as we view more objects, the final
-        # time we update an entry for each day will be the total
-        # number of objects seen at the the end of the day.
-        date_to_count[date] = num_seen_in_program
+        # check if this line corresponds to the program of interest and
+        # has an observation
+        if program == program_name and len(fields) == 4:
+            observation_id = fields[3]
+            obs_date = parse_date(observation_id)
+            if obs_date not in date_to_count:
+                date_to_count[obs_date] = 0
+            date_to_count[obs_date] += 1
     return date_to_count
 
 
@@ -64,24 +55,10 @@ def fill_missing_dates(date_to_count):
     for date in dates:
         str_date = date.strftime("%Y-%m-%d")
         if str_date in date_to_count:
-            curr_count = date_to_count[str_date]
+            curr_count += date_to_count[str_date]
         str_dates.append(str_date)
         counts.append(curr_count)
     return (str_dates, counts)
-
-
-def trim_range(dates, counts):
-    # only save last 0
-    last_zero_idx = 0
-    for i in range(0, len(dates)):
-        if counts[i] == 0:
-            last_zero_idx = i
-    last_high_idx = len(dates)
-    for i in range(len(dates)-1, 0, -1):
-        if counts[i] == counts[-1]:
-            last_high_idx = i+1
-    return (dates[last_zero_idx:last_high_idx],
-            counts[last_zero_idx:last_high_idx])
 
 
 def save_plot(dates, counts, program_name, output_file):
@@ -108,21 +85,18 @@ if __name__ == "__main__":
     parser = OptionParser()
     parser.add_option("-p", "--program_file", dest="program_file",
                       help="file with programs", metavar="FILE")
-    parser.add_option("-b", "--observation_file", dest="observation_file",
-                      help="file with observations", metavar="FILE")
+    #parser.add_option("-b", "--observation_file", dest="observation_file",
+    #                  help="file with observations", metavar="FILE")
     parser.add_option("-n", "--program_name", dest="program_name",
                       help="program name to graph")
     parser.add_option("-o", "--output_file", dest="output_file",
                       help="graphics file to create", metavar="FILE")
     (options, args) = parser.parse_args()
     if not (options.program_file and
-            options.observation_file and
             options.program_name and
             options.output_file):
         parser.error("all options must be set.  Run with -h to see usage.")
 
-    items = get_item_names(options.program_file, options.program_name)
-    dc = get_dates_seen(options.observation_file, items)
+    dc = get_dates_seen(options.program_file, options.program_name)
     (dates, counts) = fill_missing_dates(dc)
-    (dates, counts) = trim_range(dates, counts)
     save_plot(dates, counts, options.program_name, options.output_file)
