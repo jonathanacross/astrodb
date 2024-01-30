6dff64ced61dda57a4411b1f2b2970aa8d96ac47
Jonathan Cross <jonathan.a.cross@gmail.com>
2024-01-29 20:31:34 -0800

    getting search on three tabs to work

--- web/objectfinder.js
+++ web/objectfinder.js
@@ -1,16 +1,12 @@
 /* jshint esversion: 6 */
 
 import { tsvToJson, keyByColumn, keyByColumnAsLists } from './tsv_utils.js'
-import { ObjectFilter } from './query.js'
+import { ObjectFilter, ObservationFilter, ProgramFilter } from './query.js'
 import { objectTypes, constellations } from './astrodata.js'
 
 let observations
 let objects
 let programs
-
-function onlyUnique (value, index, self) {
-  return self.indexOf(value) === index
-}
 
 function createObjectListingTable (objectIds, columns) {
   const objTable = document.createElement('table')
@@ -80,7 +76,7 @@ function showObjectList (objectIds, showAsText) {
   // resultCount.textContent = "Found " + object_ids.length + " matching objects in " + observation_ids.length + " observations.";
   resultsHeader.appendChild(resultCount)
 
-  const resultsArea = document.getElementById('results_table')
+  const resultsArea = document.getElementById('results')
 
   // clear old results
   while (resultsArea.hasChildNodes()) {
@@ -98,119 +94,101 @@ function showObjectList (objectIds, showAsText) {
   }
 }
 
+function showObservations(observation_ids) {
+  // count of observations
+  let resultsHeader = document.getElementById("results_header");
+  // clear old header
+  while (resultsHeader.hasChildNodes()) {
+      resultsHeader.removeChild(resultsHeader.lastChild);
+  }
+  let resultCount = document.createElement("p");
+  //resultCount.textContent = "Found " + object_ids.length + " matching objects in " + observation_ids.length + " observations.";
+  resultCount.textContent = "Found " + observation_ids.length + " observations.";
+  resultsHeader.appendChild(resultCount);
+
+  let resultsArea = document.getElementById("results");
+
+  // clear old results
+  while (resultsArea.hasChildNodes()) {
+    resultsArea.removeChild(resultsArea.lastChild);
+  }
+
+  // add new results
+  for (const observation_id of observation_ids) {
+      const obs = observations[observation_id];
+      const fragment = document.createDocumentFragment();
+      if (obs == null) {
+          let obsDiv = document.createElement("div");
+          obsDiv.className = "observation";
+
+          let errDiv = document.createElement("div");
+          errDiv.className = "error";
+          errDiv.textContent = "Error: can't find observation id '" + observation_id + "'";
+
+          obsDiv.appendChild(errDiv);
+          fragment.appendChild(obsDiv);
+      } else {
+          const obsDiv = document.createElement("div");
+          obsDiv.className = "observation";
+
+          const notesDiv = document.createElement("div");
+          notesDiv.className = "notes";
+          const objinfo = document.createElement("div");
+          //setObjectInfo(obs, objinfo);
+
+          const notesList = document.createElement("ul");
+          const attributes = ["Date", "Location", "Scope", "Seeing", "Trans", "Time", "Eyepiece", "Mag", "Phase"];
+          for (const attr of attributes) {
+              if (obs[attr] !== "") {
+                  const li = document.createElement("li");
+                  li.textContent = attr + ": " + obs[attr];
+                  notesList.appendChild(li);
+              }
+          }
+          notesDiv.appendChild(objinfo);
+          notesDiv.appendChild(notesList);
+          if (obs.Notes != null) {
+              let description = document.createElement("p");
+              description.textContent = obs.Notes;
+              notesDiv.appendChild(description);
+          }
+
+          let sketchDiv = document.createElement("div");
+          sketchDiv.className = "sketch";
+          let sketch = document.createElement("img");
+          sketch.src = "data/sketches/" + obs["#id"] + ".jpg";
+          sketchDiv.appendChild(sketch);
+
+          obsDiv.appendChild(notesDiv);
+          obsDiv.appendChild(sketchDiv);
+          fragment.appendChild(obsDiv);
+      }
+
+      resultsArea.appendChild(fragment);
+  }
+}
+
 function doProgramQuery () {
-  const programpicker = document.getElementById('program')
-  const program_name = programpicker.options[programpicker.selectedIndex].value
+  const filter = new ProgramFilter()
+  filter.setProgramNameIs(document.getElementById('program_program_name').value)
 
-  let newquery = 'show=program'
-  newquery += '&name=' + encodeURIComponent(program_name)
-
+  let newquery = 'show=program' + filter.getUrlParameters()
   history.replaceState(null, '', window.location.origin + window.location.pathname + '?' + newquery)
-  showObjectsForSelectedProgram(program_name)
-}
 
-function ObjectNameContains (obj, name) {
-  const names = obj.Names.toLowerCase().split('/')
-  for (const n of names) {
-    if (n.includes(name)) {
-      return true
-    }
-  }
-  return false
-}
+  const matchingObservationIds = filter.getMatchingObservationIds(programs)
 
-function ObsDateContains (obs, month) {
-  return obs.Date.includes(month)
-}
-
-function ObjectTypeContains (obj, type) {
-  const types = obj.Type.split('+')
-  for (const t of types) {
-    if (t === type) {
-      return true
-    }
-  }
-  return false
-}
-
-function ObjectConIs (obj, con) {
-  return obj.Con === con
-}
-
-function getSortFunction (sortMethod) {
-  if (sortMethod === 'name') {
-    return function (x, y) {
-      // first sort by name
-      if (x.obj_name !== y.obj_name) {
-        return x.obj_name.localeCompare(y.obj_name, undefined, { numeric: true, sensitivity: 'base' })
-      }
-      // fall back to observation date/number
-      return x.obs_id.localeCompare(y.obs_id)
-    }
-  } else if (sortMethod === 'date') {
-    return function (x, y) {
-      // obs_id has date-obs#-name, so this includes date then observation number
-      return x.obs_id.localeCompare(y.obs_id)
-    }
-  } else if (sortMethod === 'ra') {
-    return function (x, y) {
-      // Sort by RA first
-      if (Math.abs(x.obj_ra - y.obj_ra) > 0.00001) {
-        return x.obj_ra - y.obj_ra
-      }
-      // For objects with no constellation/RA, sort by name
-      if (x.obj_name !== y.obj_name) {
-        return x.obj_name.localeCompare(y.obj_name, undefined, { numeric: true, sensitivity: 'base' })
-      }
-      // And finally fall back to date/observation number (encapsulated by obs_id)
-      return x.obs_id.localeCompare(y.obs_id)
-    }
-  } else {
-    return function (x, y) {
-      // First sort by constellation
-      const conToRa = Object.fromEntries(constellations.map(c => [c.shortName, c.ra]))
-      if (x.obj_con !== y.obj_con) {
-        return conToRa[x.obj_con].localeCompare(conToRa[y.obj_con])
-      }
-      // Within constellation sort by RA
-      if (Math.abs(x.obj_ra - y.obj_ra) > 0.00001) {
-        return x.obj_ra - y.obj_ra
-      }
-      // For objects with no constellation/RA, sort by name
-      if (x.obj_name !== y.obj_name) {
-        return x.obj_name.localeCompare(y.obj_name, undefined, { numeric: true, sensitivity: 'base' })
-      }
-      // And finally fall back to date/observation number (encapsulated by obs_id)
-      return x.obs_id.localeCompare(y.obs_id)
-    }
-  }
-}
-
-function raStringToFloat (raStr) {
-  const regex = /\d+/g
-  const found = raStr.match(regex)
-  if (found !== null && found.length === 3) {
-    const hrs = parseFloat(found[0])
-    const mins = parseFloat(found[1])
-    const secs = parseFloat(found[2])
-    return hrs + (mins / 60.0) + (secs / 3600.0)
-  } else {
-    // This will be the case for things without a position,
-    // e.g., planets and comets.
-    // Give a high value to put at the end.
-    return 99
-  }
+  showObservations(matchingObservationIds, objects);
 }
 
 function doObjectQuery () {
   const listType = document.querySelector('input[name="listtype"]:checked').value
 
   const filter = new ObjectFilter()
-  filter.setNameLike(document.getElementById('name').value)
-  filter.setTypeIs(document.getElementById('type').value)
-  filter.setConIs(document.getElementById('constellation').value)
-  filter.setRaRange(document.getElementById('ra_min').value,
-    document.getElementById('ra_max').value)
+  filter.setNameLike(document.getElementById('object_object_name').value)
+  filter.setTypeIs(document.getElementById('object_object_type').value)
+  filter.setConIs(document.getElementById('object_constellation').value)
+  filter.setRaRange(document.getElementById('object_ra_min').value,
+    document.getElementById('object_ra_max').value)
 
   let newquery = 'show=objects' + filter.getUrlParameters()
   newquery += '&mode=' + encodeURIComponent(listType)
@@ -223,60 +201,25 @@ function doObjectQuery () {
   showObjectList(matchingObjectIds, showAsText)
 }
 
-function showObjectsForObjectQuery (nameQuery, typeQuery, conQuery, dateQuery, sortMethod) {
-  const observation_ids_and_matching_obj_info = []
-  let allMatchingObjectIds = []
-  for (const [observationId, observation] of Object.entries(observations)) {
-    const matchingObjects = []
-    const objids = observation.ObjIds.split('|')
-    const objs = objids.map(objId => objects[objId])
-    for (const obj of objs) {
-      let objectMatches = true
-      if (nameQuery !== null && !ObjectNameContains(obj, nameQuery.toLowerCase())) {
-        objectMatches = false
-      }
-      if (conQuery !== null && (
-        obj.Con == null || !ObjectConIs(obj, conQuery))) {
-        objectMatches = false
-      }
-      if (typeQuery !== null && (
-        obj.Type == null || !ObjectTypeContains(obj, typeQuery))) {
-        objectMatches = false
-      }
-      if (dateQuery !== null && !ObsDateContains(observation, dateQuery)) {
-        objectMatches = false
-      }
-      if (objectMatches) {
-        matchingObjects.push(obj)
-      }
-    }
+function doObservationQuery () {
+  const listType = document.querySelector('input[name="listtype"]:checked').value
 
-    if (matchingObjects.length > 0) {
-      const objName = matchingObjects[0].Names.split('/')[0]
-      const objCon = matchingObjects[0].Con
-      const objRa = raStringToFloat(matchingObjects[0].RA)
-      const obsDate = observation.Date
-      observation_ids_and_matching_obj_info.push({
-        obs_id: observationId,
-        obj_name: objName,
-        obj_con: objCon,
-        obj_ra: objRa,
-        obs_date: obsDate
-      })
-      allMatchingObjectIds = allMatchingObjectIds.concat(
-        matchingObjects.map(mo => mo['#id']))
-    }
-  }
+  const filter = new ObservationFilter()
+  filter.setNameLike(document.getElementById('observation_object_name').value)
+  filter.setTypeIs(document.getElementById('observation_object_type').value)
+  filter.setConIs(document.getElementById('observation_constellation').value)
+  filter.setDateLike(document.getElementById('observation_date').value)
 
-  // sort observations
-  const sortFun = getSortFunction(sortMethod)
-  observation_ids_and_matching_obj_info.sort(sortFun)
-  // and strip down to just obs ids
-  observation_ids = observation_ids_and_matching_obj_info.map(x => x.obs_id)
+  let newquery = 'show=objects' + filter.getUrlParameters()
+  newquery += '&mode=' + encodeURIComponent(listType)
 
-  const deduped_matching_obj_ids = allMatchingObjectIds.filter(onlyUnique)
-  showObservations(observation_ids, deduped_matching_obj_ids)
+  history.replaceState(null, '', window.location.origin + window.location.pathname + '?' + newquery)
+
+  const matchingObservationIds = filter.getMatchingObservationIds(observations, objects)
+
+  showObservations(matchingObservationIds, objects);
 }
+
 
 function showQuery(search_query_type) {
   // Get all elements with class="tabcontent" and hide them
@@ -311,58 +254,45 @@ function showResults () {
   const urlParams = new URLSearchParams(queryString)
 
   showObjectList(Object.keys(objects))
-
-  // const show_query = urlParams.get("show");
-  // if (show_query == "program") {
-  //    const program_name = urlParams.get("name");
-  //    showObjectsForSelectedProgram(program_name);
-  // } else if (show_query == "objects") {
-  //    const name_query = urlParams.get("name");
-  //    const type_query = urlParams.get("type");
-  //    const con_query = urlParams.get("con");
-  //    const date_query = urlParams.get("date");
-  //    const list_type = urlParams.has("sortby") ? urlParams.get("sortby") : "name";
-  //    showObjectsForObjectQuery(name_query, type_query, con_query, date_query, list_type);
-  // }
 }
 
 function updateControlsFromSearchParams () {
-  const queryString = window.location.search
-  const urlParams = new URLSearchParams(queryString)
+  // TODO: implmement
 
-  const show_query = urlParams.get('show')
-  if (show_query == 'program') {
-    const program_name = urlParams.get('name')
-    const programpicker = document.getElementById('program')
-    programpicker.value = program_name
-  } else if (show_query == 'objects') {
-    const name_query = urlParams.get('name')
-    const type_query = urlParams.get('type')
-    const con_query = urlParams.get('con')
-    const date_query = urlParams.get('date')
-    const list_type = urlParams.has('sortby') ? urlParams.get('sortby') : 'name'
-    const namequery_element = document.getElementById('name')
-    const type_element = document.getElementById('type')
-    const con_element = document.getElementById('constellation')
-    const date_element = document.getElementById('dateobs')
-    const list_type_element = document.getElementById(list_type + '_radio')
-    namequery_element.value = name_query
-    type_element.value = type_query
-    con_element.value = con_query
-    // date_element.value = date_query
-    list_type_element.checked = true
-  }
+  // const queryString = window.location.search
+  // const urlParams = new URLSearchParams(queryString)
+
+  // const show_query = urlParams.get('show')
+  // if (show_query == 'program') {
+  //   const program_name = urlParams.get('name')
+  //   const programpicker = document.getElementById('program')
+  //   programpicker.value = program_name
+  // } else if (show_query == 'objects') {
+  //   const name_query = urlParams.get('name')
+  //   const type_query = urlParams.get('type')
+  //   const con_query = urlParams.get('con')
+  //   const date_query = urlParams.get('date')
+  //   const list_type = urlParams.has('sortby') ? urlParams.get('sortby') : 'name'
+  //   const namequery_element = document.getElementById('name')
+  //   const type_element = document.getElementById('type')
+  //   const con_element = document.getElementById('constellation')
+  //   const date_element = document.getElementById('dateobs')
+  //   const list_type_element = document.getElementById(list_type + '_radio')
+  //   namequery_element.value = name_query
+  //   type_element.value = type_query
+  //   con_element.value = con_query
+  //   // date_element.value = date_query
+  //   list_type_element.checked = true
+  // }
 }
 
 function setupControls () {
-  const programpicker = document.getElementById('program')
-  // for (const [program_name, _] of Object.entries(programs).sort()) {
-  //    let option = document.createElement("option");
-  //    option.text = program_name;
-  //    programpicker.add(option);
-  // }
-  // let programbutton = document.getElementById("programshow");
-  // programbutton.addEventListener("click", doProgramQuery);
+  const programpicker = document.getElementById('program_program_name')
+  for (const [program_name, _] of Object.entries(programs).sort()) {
+    let option = document.createElement("option");
+    option.text = program_name;
+    programpicker.add(option);
+  }
 
   let cons = new Set()
   for (const [_, obs] of Object.entries(observations)) {
@@ -395,8 +325,14 @@ function setupControls () {
     }
   }
 
-  const objbutton = document.getElementById('objshow')
-  objbutton.addEventListener('click', doObjectQuery)
+  const objectShowButton = document.getElementById('object_show')
+  objectShowButton.addEventListener('click', doObjectQuery)
+
+  const programShowButton = document.getElementById('program_show')
+  programShowButton.addEventListener('click', doProgramQuery)
+
+  const observationShowButton = document.getElementById('observation_show')
+  observationShowButton.addEventListener('click', doObservationQuery)
 
   const view_program_tab_element = document.getElementById('view_program_tab');
   view_program_tab_element.addEventListener('click', () => showQuery('view_program'));
@@ -440,11 +376,11 @@ function JoinObjects() {
       if (!containsKey(objects, objId)) {
         throw Error('Observation ' + JSON.stringify(observation) + ' has objectId ' + objId + " that doesn't appear in any object");
       }
-      let currObject = objects[objId];
+      const currObject = objects[objId];
       if (!containsKey(currObject, 'observation_ids')) {
-        currObject['observation_ids'] = [];
+        currObject.observation_ids = [];
       }
-      currObject['observation_ids'].push(observation['#id']);
+      currObject.observation_ids.push(observation['#id']);
     }
   }
 
--- web/query.js
+++ web/query.js
@@ -1,5 +1,9 @@
 function nullOrEmpty (str) {
-  return str !== null && str !== ''
+  return str === null || str === ''
+}
+
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
 }
 
 export class ObjectFilter {
@@ -57,7 +61,7 @@ export class ObjectFilter {
   getUrlParameters () {
     let params = ''
     if (this.#nameIs !== null) {
-      params += '&nameIs=' + encodeURIComponent(this.nameIs)
+      params += '&nameIs=' + encodeURIComponent(this.#nameIs)
     }
     if (this.#nameLike !== null) {
       params += '&nameLike=' + encodeURIComponent(this.#nameLike)
@@ -80,32 +84,31 @@ export class ObjectFilter {
   raIsInRange(ra) {
     // no RA defined
     if (nullOrEmpty(ra)) {
-        return true
+      return true
     }
 
     // no RA filter defined
     if (this.#raMin === null && this.#raMax === null) {
-        return true
+      return true
     }
 
     // only max RA defined
     if (this.#raMin === null) {
-        return this.#raMax >= ra
+      return this.#raMax >= ra
     }
 
     // only min RA defined
     if (this.#raMax === null) {
-        return this.#raMin <= ra
+      return this.#raMin <= ra
     }
 
     // Both min and max RA defined.  If the min and max are reversed, then
     // reverse the checks, to handle objects crossing the 24-0 RA line.
     if (this.#raMin < this.#raMax) {
-        return this.#raMin <= ra && ra <= this.#raMax
+      return this.#raMin <= ra && ra <= this.#raMax
     } else {
-        return this.#raMin >= ra && ra >= this.#raMax
+      return this.#raMin >= ra && ra >= this.#raMax
     }
-
   }
 
   #objectMatches (object) {
@@ -123,7 +126,7 @@ export class ObjectFilter {
       return false
     }
     if (!this.raIsInRange(object.Ra)) {
-        return false
+      return false
     }
     return true
   }
@@ -136,5 +139,123 @@ export class ObjectFilter {
       }
     }
     return results
+  }
+}
+
+export class ObservationFilter {
+  #nameIs = null
+  #nameLike = null
+  #typeIs = null
+  #conIs = null
+  #dateLike = null
+
+  setNameIs (nameIs) {
+    if (!nullOrEmpty(nameIs)) {
+      this.#nameIs = nameIs.toLowerCase()
+    }
+  }
+
+  setNameLike (nameLike) {
+    if (!nullOrEmpty(nameLike)) {
+      this.#nameLike = nameLike.toLowerCase()
+    }
+  }
+
+  setTypeIs (typeIs) {
+    if (!nullOrEmpty(typeIs)) {
+      this.#typeIs = typeIs
+    }
+  }
+
+  setConIs (conIs) {
+    if (!nullOrEmpty(conIs)) {
+      this.#conIs = conIs
+    }
+  }
+
+  setDateLike (dateLike) {
+    if (!nullOrEmpty(dateLike)) {
+      this.#dateLike = dateLike.toLowerCase()
+    }
+  }
+
+  getUrlParameters () {
+    let params = ''
+    if (this.#nameIs !== null) {
+      params += '&nameIs=' + encodeURIComponent(this.#nameIs)
+    }
+    if (this.#nameLike !== null) {
+      params += '&nameLike=' + encodeURIComponent(this.#nameLike)
+    }
+    if (this.#typeIs !== null) {
+      params += '&typeIs=' + encodeURIComponent(this.#typeIs)
+    }
+    if (this.#conIs !== null) {
+      params += '&conIs=' + encodeURIComponent(this.#conIs)
+    }
+    if (this.#dateLike !== null) {
+      params += '&dateLike=' + encodeURIComponent(this.#dateLike)
+    }
+    return params
+  }
+
+  #observationMatches (observation) {
+    //const objectNames = observation.Names.toLowerCase().split('/')
+    // if (this.#nameIs !== null && objectNames.every((name) => this.#nameIs !== name)) {
+    //   return false
+    // }
+    // if (this.#nameLike !== null && objectNames.every((name) => !name.includes(this.#nameLike))) {
+    //   return false
+    // }
+    // if (this.#typeIs !== null && this.#typeIs !== observation.Type) {
+    //   return false
+    // }
+    // if (this.#conIs !== null && this.#conIs !== observation.Con) {
+    //   return false
+    // }
+    if (this.#dateLike !== null && !observation.Date.includes(this.#dateLike)) {
+      return false
+    }
+    return true
+  }
+
+  getMatchingObservationIds (observations) {
+    const results = []
+    for (const [objId, obj] of Object.entries(observations)) {
+      if (this.#observationMatches(obj)) {
+        results.push(objId)
+      }
+    }
+    return results
+  }
+}
+
+export class ProgramFilter {
+  #programNameIs = null
+
+  setProgramNameIs (programNameIs) {
+    if (!nullOrEmpty(programNameIs)) {
+      this.#programNameIs = programNameIs
+    }
+  }
+
+  getUrlParameters () {
+    let params = ''
+    if (this.#programNameIs !== null) {
+      params += '&programName=' + encodeURIComponent(this.#programNameIs)
+    }
+    return params
+  }
+
+  getMatchingObservationIds (programs) {
+    const entries = programs[this.#programNameIs];
+    const obsIds = entries
+      .filter(o => o.observationId !== '')
+      .map(o => o.observationId)
+      .filter(onlyUnique);
+
+    // TODO: extend to include matching objects, so we can show the count in the UI
+
+    return obsIds;
   }
 }
